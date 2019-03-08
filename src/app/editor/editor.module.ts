e3912344d2415f6be563907ac00b7036f47bba2b
wubo <jack_wu@alldone.com.cn>
2019-03-08 17:33:02 +0800

    3.8

--- src/app/editor/editor.module.ts
+++ src/app/editor/editor.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { EditorRoutingModule } from './editor-routing.module';
 import { EditorComponent } from './editor.component';
+import { StageComponent } from './stage/stage.component';
 
 @NgModule({
   declarations: [
-    EditorComponent
+    EditorComponent,
+    StageComponent
   ],
   imports: [
     CommonModule,
